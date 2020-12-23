45fa90635762af0881c39dce9b90ceade8fb8a34
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2020-12-23 19:47:45 +0700

    mengikat UI menggunakan Binding

--- Praktikum9/app/src/main/java/com/puspawahyuningtias_18102137/praktikum9/MainActivity.kt
+++ Praktikum9/app/src/main/java/com/puspawahyuningtias_18102137/praktikum9/MainActivity.kt
@@ -2,10 +2,14 @@ package com.puspawahyuningtias_18102137.praktikum9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.puspawahyuningtias_18102137.praktikum9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = getString(R.string.main_title)
     }
 }
