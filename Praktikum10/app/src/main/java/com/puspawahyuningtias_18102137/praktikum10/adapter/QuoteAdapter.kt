1658b3dbaa0335aa6480b6aa426278d813267280
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2021-01-02 11:19:42 +0700

    menambahkan aksi ketika cardview ditekan

--- Praktikum10/app/src/main/java/com/puspawahyuningtias_18102137/praktikum10/adapter/QuoteAdapter.kt
+++ Praktikum10/app/src/main/java/com/puspawahyuningtias_18102137/praktikum10/adapter/QuoteAdapter.kt
@@ -1,13 +1,18 @@
 package com.puspawahyuningtias_18102137.praktikum10.adapter
 
 import android.app.Activity
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.puspawahyuningtias_18102137.praktikum10.QuoteAddUpdateActivity
 import com.puspawahyuningtias_18102137.praktikum10.R
 import com.puspawahyuningtias_18102137.praktikum10.data.Quote
 import com.puspawahyuningtias_18102137.praktikum10.databinding.ItemQuoteBinding
+import com.puspawahyuningtias_18102137.praktikum10.helper.EXTRA_POSITION
+import com.puspawahyuningtias_18102137.praktikum10.helper.EXTRA_QUOTE
+import com.puspawahyuningtias_18102137.praktikum10.helper.REQUEST_UPDATE
 import com.puspawahyuningtias_18102137.praktikum10.helper.categoryList
 
 class QuoteAdapter(private val activity: Activity):
@@ -31,6 +36,13 @@ class QuoteAdapter(private val activity: Activity):
             binding.tvItemCategory.text = categoryList[quote.category!!.toInt()]
             binding.tvItemDate.text = quote.date
             binding.tvItemDescription.text = quote.description
+            binding.cvItemQuote.setOnClickListener{
+                val intent = Intent(activity, QuoteAddUpdateActivity::class.java)
+                intent.putExtra(EXTRA_POSITION, position)
+                intent.putExtra(EXTRA_QUOTE, quote)
+                activity.startActivityForResult(intent, REQUEST_UPDATE)
+            }
+
         }
     }
     fun addItem(quote: Quote) {
