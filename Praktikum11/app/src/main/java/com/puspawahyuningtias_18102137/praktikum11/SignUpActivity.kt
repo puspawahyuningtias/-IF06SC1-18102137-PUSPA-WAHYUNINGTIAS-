c5835b3a957984663e993e3bde470405b90d516e
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2021-01-13 23:04:55 +0700

    Menambahkan Activity Sign Up

--- Praktikum11/app/src/main/java/com/puspawahyuningtias_18102137/praktikum11/SignUpActivity.kt
+++ Praktikum11/app/src/main/java/com/puspawahyuningtias_18102137/praktikum11/SignUpActivity.kt
@@ -1,6 +1,5 @@
 package com.puspawahyuningtias_18102137.praktikum11
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
@@ -9,51 +8,42 @@ import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.puspawahyuningtias_18102137.praktikum11.databinding.ActivitySignInBinding
+import com.puspawahyuningtias_18102137.praktikum11.databinding.ActivitySignUpBinding
 
-class SignInActivity : AppCompatActivity(), View.OnClickListener {
+class SignUpActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var auth: FirebaseAuth
-    private lateinit var binding: ActivitySignInBinding
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignInBinding.inflate(layoutInflater)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnSign.setOnClickListener(this)
-        binding.tvSignUp.setOnClickListener(this)
-        binding.btnEmail.setOnClickListener(this)
-        binding.btnPhone.setOnClickListener(this)
+        binding.btnCreateAccount.setOnClickListener(this)
         auth = Firebase.auth
     }
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.btnSign -> {
-                signIn(binding.inputEmail.text.toString(),
+            R.id.btnCreateAccount -> {
+                createAccount(binding.inputEmail.text.toString(),
                     binding.inputPassword.text.toString())
-            }
-            R.id.tvSignUp -> {
-                val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
-                startActivity(intent)
             }
         }
     }
-    private fun signIn(email: String, password: String) {
+    private fun createAccount(email: String, password: String) {
         if (!validateForm()) {
             return
         }
-        auth.signInWithEmailAndPassword(email, password)
+        auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
+                    Toast.makeText(baseContext, "Create User Success.",
+                        Toast.LENGTH_SHORT).show()
                     finish()
-                    val intent = Intent(this@SignInActivity,
-                        MainActivity::class.java)
-                    startActivity(intent)
                 } else {
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
                 }
             }
     }
-
     private fun validateForm(): Boolean {
         var valid = true
         val email = binding.inputEmail.text.toString()
@@ -71,14 +61,5 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
             binding.inputPassword.error = null
         }
         return valid
-    }
-    public override fun onStart() {
-        super.onStart()
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            val intent = Intent(this@SignInActivity, MainActivity::class.java)
-            startActivityForResult(intent, 1)
-            finish()
-        }
     }
 }
