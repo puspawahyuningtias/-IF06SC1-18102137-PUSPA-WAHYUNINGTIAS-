824ffeac33f55f239ec12ecc5f38e7ebbcb7e7e6
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2021-02-06 10:35:53 +0700

    konfigurasi token

---
+++ Praktikum14/app/src/main/java/com/puspawahyuningtias_18102137/praktikum14/model/Token.kt
@@ -0,0 +1,9 @@
+package com.puspawahyuningtias_18102137.praktikum14.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Token (
+    var token: String? = null
+): Parcelable
