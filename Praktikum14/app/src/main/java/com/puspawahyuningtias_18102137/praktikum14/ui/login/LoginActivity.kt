35d8dbfe9f4b0dc45796014325806aaa64abfcc5
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2021-02-06 10:27:14 +0700

    add LoginActivity

---
+++ Praktikum14/app/src/main/java/com/puspawahyuningtias_18102137/praktikum14/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.puspawahyuningtias_18102137.praktikum14.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.puspawahyuningtias_18102137.praktikum14.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
